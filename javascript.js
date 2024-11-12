2d491721f79d5149301abf42d5ec6a3e17e9c0bc
AlessandroQuerci <isalessandroquerci@gmail.com>
2024-11-12 18:04:40 +0100

    FINE

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@ const saveBtn = document.getElementById("saveUser");
 const deleteBtn = document.getElementById("deleteUser");
 const showResult = document.getElementById("userNameRecorder");
 const userForm = document.querySelector("form");
-
+const showedTimer = document.getElementById("contatore");
+console.log(showedTimer);
 window.addEventListener("load", () => {
   const savedUserName = localStorage.getItem("userName");
   if (savedUserName) {
@@ -23,3 +24,13 @@ deleteBtn.addEventListener("click", () => {
   localStorage.removeItem("userName");
   showResult.innerText = "";
 });
+
+const updateTimer = function () {
+  let time = 0;
+  setInterval(function () {
+    time++;
+    showedTimer.innerText = time;
+    sessionStorage.setItem("time", time);
+  }, 1000);
+};
+updateTimer();
